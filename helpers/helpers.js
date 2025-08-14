f775cfd10ca8e339cbf6ca65795a3590ff90fdcb
Eros <eros.gear@gmail.com>
2025-08-13 22:58:26 -0700

    segunda entrega depsues de revision

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -4,45 +4,44 @@ const userPath = path.join(__dirname, "../data/users.json");
 const User = require('../models/users');
 const Card = require('../models/cards');
 
-const getUsers = (req, res)=>{
+const getUsers = (req, res, next)=>{
   User.find({})
-  .orFail()
+  .orFail()//controlador de errores global esta en app.js como middleware
   .then(users => res.json(users))
   .catch(next);
 }
-const getCards = (req, res)=>{
+const getCards = (req, res, next)=>{
   Card.find({})
-  .orFail()
+  .orFail()//controlador de errores global esta en app.js como middleware
   .then((cards)=>{console.log(cards)})
   .then(cards => res.json(cards))
   .catch(next);
 }
 
-const getUser = (req,res)=>{
+const getUser = (req,res, next)=>{
   const { id } = req.params;
   User.findById(id)
-  .orFail()
+  .orFail()//controlador de errores global esta en app.js como middleware
   .then(user => res.json(user))
   .catch(next);
 }
 
-const createUser = (req, res)=>{
+const createUser = (req, res, next)=>{
   const {name, about, avatar} = req.body;
   User.create({name, about, avatar})
-  .orFail()
-  .then(user => res.json(user))
-    .catch(next);
-}
-
-const updateUser = (req, res)=>{
-  const {name, about, avatar} = req.body;
-  User.findByIdAndUpdate(req.user._id, {name, about, avatar}, {new: true})
-  .orFail()
   .then(user => res.json(user))
   .catch(next);
 }
 
-const updateAvatar = (req, res)=>{
+const updateUser = (req, res, next)=>{
+  const {name, about, avatar} = req.body;
+  User.findByIdAndUpdate(req.user._id, {name, about, avatar}, {new: true})
+  .orFail()//controlador de errores global esta en app.js como middleware
+  .then(user => res.json(user))
+  .catch(next);
+}
+
+const updateAvatar = (req, res, next)=>{
   const {avatar} = req.body;
   User.findByIdAndUpdate(req.user._id, {avatar}, {new: true})
   .orFail()
@@ -50,34 +49,33 @@ const updateAvatar = (req, res)=>{
   .catch(next);
 }
 
-const createCard = (req, res)=>{
+const createCard = (req, res, next)=>{
   const {name, link} = req.body;
   Card.create({name, link})
-  .orFail()
   .then(card => res.json(card))
     .catch(next);
 }
 
-const deleteCard = (req, res)=>{
+const deleteCard = (req, res, next)=>{
   const { cardId } = req.params;
   Card.findByIdAndDelete(cardId)
-  .orFail()
+  .orFail()//controlador de errores global esta en app.js como middleware
   .then(card => res.json(card))
   .catch(next);
 }
 
-const likeCard = (req, res)=>{
+const likeCard = (req, res, next)=>{
   const { cardId } = req.params;
   Card.findByIdAndUpdate(cardId, {$addToSet: {likes: req.user._id}}, {new: true})
-  .orFail()
+  .orFail()//controlador de errores global esta en app.js como middleware
   .then(card => res.json(card))
   .catch(next);
 }
 
-const dislikeCard = (req, res)=>{
+const dislikeCard = (req, res, next)=>{
   const { cardId } = req.params;
   Card.findByIdAndUpdate(cardId, {$pull: {likes: req.user._id}}, {new: true})
-  .orFail()
+  .orFail()//controlador de errores global esta en app.js como middleware
   .then(card => res.json(card))
   .catch(next);
 }
